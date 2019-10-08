eecb93f25d505e17e5d546cd032a26ef673aa938
yomedia <you@example.com>
2019-10-08 18:04:51 +0700

    done

--- index.js
+++ index.js
@@ -3,15 +3,14 @@ require('dotenv').config()//***your magic line
 const Sequelize = require('sequelize');
 
 // Option 1: Passing parameters separately
-const sequelize = new Sequelize(
-    process.env.db_name,
-    process.env.db_user,
-    process.env.db_password,
-    {
-        host: process.env.db_host,
-        dialect: 'postgres'
-    });
-
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
+});
 sequelize
     .authenticate()
     .then(() => {
@@ -20,3 +19,51 @@ sequelize
     .catch(err => {
         console.error('Unable to connect to the database:', err);
     });
+
+console.log('hello world 2');
+
+// test
+
+
+
+const express = require('express');
+const app = express();
+const User = sequelize.define('user', {
+    // attributes
+    firstName: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    lastName: {
+      type: Sequelize.STRING
+      // allowNull defaults to true
+    }
+  }, {
+    // options
+  });
+app.get('/', async function (req, res) {
+    console.log('call');
+   
+
+    await User.sync({ force: true }).then(() => {
+        // Now the `users` table in the database corresponds to the model definition
+        return User.create({
+            firstName: 'John',
+            lastName: 'Hancock'
+        });
+    });
+    await User.findAll().then(users => {
+        console.log("All users:", JSON.stringify(users, null, 4));
+        res.json(users)
+    });
+ 
+});
+
+app.listen(3001, function () {
+  console.log('Example app listening on port 3001!');
+});
+
+
+
+
+
